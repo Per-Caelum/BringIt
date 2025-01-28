8f8aaef5efb26583a3aab08f9a1ca9fe7577d339
Your Name <jasonbataillon@gmail.com>
2025-01-28 11:41:21 -0500

    Fixed some things with registering

---
+++ prisma/index.js
@@ -0,0 +1,26 @@
+const bcrypt = require('bcrypt');
+const { PrismaClient } = require('@prisma/client');
+
+// Created model for user login credentials without changing database schema
+const prisma = new PrismaClient().$extends({
+  model: {
+    user: {
+      async register(email, password) {
+        const hash = await bcrypt.hash(password, 10);
+        const user = await prisma.user.create({
+          data: { email, password: hash },
+        });
+        return user;
+      },
+      async login(email, password) {
+        const user = await prisma.user.findUniqueOrThrow({
+          where: { email },
+        });
+        const valid = await bcrypt.compare(password, user.password);
+        if (!valid) throw Error('Invalid Password');
+        return user;
+      },
+    },
+  },
+});
+module.exports = prisma;
